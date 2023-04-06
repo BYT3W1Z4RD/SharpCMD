6dbb08eac93d1c6836a3a0b81ac364c6fd2c9c05
Byte Wizard <122925084+BYT3W1Z4RD@users.noreply.github.com>
2023-04-06 19:20:37 +0100

    Add files via upload

---
+++ SharpCMD.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SharpCMD
+{
+    partial class SharpCMD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SharpCMD));
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.outputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputTextBox.Font = new System.Drawing.Font("Consolas", 11F);
+            this.outputTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.outputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(711, 313);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.inputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.inputTextBox.Font = new System.Drawing.Font("Consolas", 11F);
+            this.inputTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.inputTextBox.Location = new System.Drawing.Point(0, 313);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(711, 25);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTextBox_KeyDown);
+            // 
+            // SharpCMD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(711, 338);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "SharpCMD";
+            this.Text = "SharpCMD";
+            this.Load += new System.EventHandler(this.CommandPromptForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+    }
+}
---
+++ SharpCMD.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+namespace SharpCMD
+{
+    public partial class SharpCMD : Form
+    {
+        public SharpCMD()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandPromptForm_Load(object sender, EventArgs e)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string welcomeMessage = $"Microsoft Windows [Version {Environment.OSVersion.Version}]"
+                + Environment.NewLine
+                + $"(c) Microsoft Corporation. All rights reserved."
+                + Environment.NewLine
+                + Environment.NewLine
+                + $"{currentDirectory}>";
+            outputTextBox.AppendText(welcomeMessage);
+            outputTextBox.ScrollToCaret();
+            inputTextBox.Focus();
+        }
+
+        private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ExecuteCommand(inputTextBox.Text.Trim());
+                inputTextBox.Text = "";
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private string ExecuteCommand(string command)
+        {
+            // Append command to output textbox
+            outputTextBox.AppendText(Environment.NewLine);
+            outputTextBox.AppendText(command);
+            outputTextBox.AppendText(Environment.NewLine);
+
+            // Create process info
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+
+            // Start the process
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+
+                // Create stream readers for output and error streams
+                using (StreamReader outputReader = process.StandardOutput)
+                using (StreamReader errorReader = process.StandardError)
+                {
+                    // Read output and error streams asynchronously
+                    Task<string> outputTask = outputReader.ReadToEndAsync();
+                    Task<string> errorTask = errorReader.ReadToEndAsync();
+
+                    // Wait for both tasks to complete
+                    Task.WaitAll(outputTask, errorTask);
+
+                    // Display output and error
+                    string output = outputTask.Result;
+                    string error = errorTask.Result;
+
+                    if (command.Equals("cls", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Clear the output textbox
+                        outputTextBox.Clear();
+
+                        // Append current working directory to output textbox
+                        outputTextBox.AppendText(Directory.GetCurrentDirectory() + ">");
+
+                        return "";
+                    }
+
+                    if (command.StartsWith("cd ", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Extract the new directory from the command
+                        string newDirectory = command.Substring(3).Trim();
+
+                        // Check if the user entered a special folder name like "temp" or "appdata"
+                        if (newDirectory.Equals("temp", StringComparison.OrdinalIgnoreCase))
+                        {
+                            // If the user entered "temp", change to the %temp% special folder
+                            string specialFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                            specialFolderPath = Path.Combine(specialFolderPath, "Temp");
+
+                            try
+                            {
+                                // Attempt to change the current directory
+                                Directory.SetCurrentDirectory(specialFolderPath);
+
+                                // Update the prompt to show the new directory
+                                string prompt = $"{Directory.GetCurrentDirectory()}> ";
+                                output = output.Replace($"{Directory.GetCurrentDirectory()}> ", prompt);
+                            }
+                            catch (Exception ex)
+                            {
+                                // Display an error message if changing the directory fails
+                                output = ex.Message;
+                            }
+                        }
+                        else if (newDirectory.Equals("appdata", StringComparison.OrdinalIgnoreCase))
+                        {
+                            // If the user entered "appdata", change to the %appdata% special folder
+                            string specialFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+                            try
+                            {
+                                // Attempt to change the current directory
+                                Directory.SetCurrentDirectory(specialFolderPath);
+
+                                // Update the prompt to show the new directory
+                                string prompt = $"{Directory.GetCurrentDirectory()}> ";
+                                output = output.Replace($"{Directory.GetCurrentDirectory()}> ", prompt);
+                            }
+                            catch (Exception ex)
+                            {
+                                // Display an error message if changing the directory fails
+                                output = ex.Message;
+                            }
+                        }
+                        else if (newDirectory.Equals("startup", StringComparison.OrdinalIgnoreCase))
+                        {
+                            // If the user entered "startup", change to the %appdata%\Microsoft\Windows\Start Menu\Programs\Startup special folder
+                            string specialFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                            specialFolderPath = Path.Combine(specialFolderPath, "Microsoft\\Windows\\Start Menu\\Programs\\Startup");
+
+                            try
+                            {
+                                // Attempt to change the current directory
+                                Directory.SetCurrentDirectory(specialFolderPath);
+
+                                // Update the prompt to show the new directory
+                                string prompt = $"{Directory.GetCurrentDirectory()}> ";
+                                output = output.Replace($"{Directory.GetCurrentDirectory()}> ", prompt);
+                            }
+                            catch (Exception ex)
+                            {
+                                // Display an error message if changing the directory fails
+                                output = ex.Message;
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                // Attempt to change the current directory
+                                Directory.SetCurrentDirectory(newDirectory);
+
+                                // Update the prompt to show the new directory
+                                string prompt = $"{Directory.GetCurrentDirectory()}> ";
+                                output = output.Replace($"{Directory.GetCurrentDirectory()}> ", prompt);
+                            }
+                            catch (Exception ex)
+                            {
+                                // Display an error message if changing the directory fails
+                                output = ex.Message;
+                            }
+                        }
+                    }
+
+
+                    // Append output and error to output textbox
+                    outputTextBox.AppendText(output);
+                    outputTextBox.AppendText(error);
+
+                    // Append current working directory to output textbox
+                    outputTextBox.AppendText(Environment.NewLine);
+                    outputTextBox.AppendText(Directory.GetCurrentDirectory() + ">");
+
+                    return output + error;
+                }
+            }
+        }
+
+    }
+}
